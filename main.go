b5c4ceb35f16b822abedb083ba67962edcbf23a5
rizky.pratama <rizky.pratama@sicepat.com>
2024-01-16 18:29:32 +0700

    feat: add connection to pg and default endpoint

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"github.com/rizama/favorite-book-tracker/database"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
 
 func main() {
-	fmt.Println("HELLO SAM")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	database.ConnectDB()
+	app := fiber.New()
+
+	routes(app)
+
+	app.Use(cors.New())
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+func routes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		response := Response{
+			Message: "Hello, World!",
+		}
+		return c.JSON(response)
+	})
 }
